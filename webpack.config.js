922c8ea4c1a7806ca7b312b0cb9de0d47a982a72
Arm0ny <63424081+Arm0ny@users.noreply.github.com>
2022-09-30 17:14:31 +0200

    webpack and lodash added

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require ("path")
+let HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+    mode: "production",
+    entry: "./js/main.js",
+    output: {
+        filename: 'main.[contenthash].js',
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    optimization: {
+        minimizer: [new OptimizeCssPlugin()]//, new TerserPlugin()]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: './html/template.html'
+    }),
+        new MiniCssExtractPlugin({filename: '[name].[contenthash].css'})
+    ],
+    module:{
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+}
